c3ea51e923292652142b7edbabef5a55b246ac99
Nonya Biz Ness <164905463+linuxfandudeguy@users.noreply.github.com>
2024-05-30 13:30:13 -0400

    Create jscore.js

---
+++ jscore.js
@@ -0,0 +1,49 @@
+// Importing jQuery
+var script1 = document.createElement('script');
+script1.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.head.appendChild(script1);
+
+// Importing Lodash
+var script2 = document.createElement('script');
+script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js';
+document.head.appendChild(script2);
+
+// Importing Moment.js
+var script3 = document.createElement('script');
+script3.src = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js';
+document.head.appendChild(script3);
+
+// Importing Axios
+var script4 = document.createElement('script');
+script4.src = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
+document.head.appendChild(script4);
+
+// Importing D3.js
+var script5 = document.createElement('script');
+script5.src = 'https://cdnjs.cloudflare.com/ajax/libs/d3/7.1.1/d3.min.js';
+document.head.appendChild(script5);
+
+// Importing Chart.js
+var script6 = document.createElement('script');
+script6.src = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js';
+document.head.appendChild(script6);
+
+// Importing Firebase SDK (for real-time database, authentication, etc.)
+var script7 = document.createElement('script');
+script7.src = 'https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js';
+document.head.appendChild(script7);
+
+// Importing Socket.io (for real-time bidirectional event-based communication)
+var script8 = document.createElement('script');
+script8.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.2.0/socket.io.min.js';
+document.head.appendChild(script8);
+
+// Importing Three.js (for 3D graphics)
+var script9 = document.createElement('script');
+script9.src = 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js';
+document.head.appendChild(script9);
+
+// Importing TensorFlow.js (for machine learning in JavaScript)
+var script10 = document.createElement('script');
+script10.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.11.0/dist/tf.min.js';
+document.head.appendChild(script10);
